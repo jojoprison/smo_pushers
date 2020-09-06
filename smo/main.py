51f776aacdadba86b574dcdf2ce3cb3f19846f10
squalorDf <egyabig2@gmail.com>
2020-09-06 23:16:50 +0300

    first

--- smo/main.py
+++ smo/main.py
@@ -1,74 +1,121 @@
 import numpy as np
 import pandas as pd
+from array import *
 
 
+# функция генерации случайных чисел в пределе [0, 1]
 def generate_r():
-    return -np.log(np.random.uniform(low=0.0, high=1.0)) * 6
+    return -np.log(np.random.uniform(low=0.0, high=1.0)) * 1
 
-class Simulation():
+
+# на вход интенсивность событий (в час)
+def flow_init(intensity):
+    # Пуассоновский поток случайных событий
+    # интенсивность потока (изделия/24 часа), λ
+    intensity = intensity / 24
+    # время потока (наблюдения) (в часах) , Tн
+    flow_time = 30
+    # затраченное время
+    time_ = 0
+    # число событий за время наблюдения, N
+    count = 0
+
+    # в какое время пришли поезда
+    events = []
+
+    while time_ <= flow_time:
+        # равномерно распределенное от 0 до 1 случайное число, r
+        r = generate_r()
+        # время появления собития, m = 1/λ
+        m = 1 / intensity
+        # интервал между случайными собитями , τ
+        time_interval = m * r
+        time_ = time_ + time_interval
+        count += 1
+
+        events.append(time_)
+
+    print(count)
+    print(events)
+
+    return events
+
+
+class Simulation:
     def __init__(self):
-        # затраты при  простое одного вагона (руб/час)
+        # затраты при простое одного вагона (руб/час)
         self.carriage_downtime_price = 60
         # затраты при простое толкача (руб/час)
         self.pusher_downtime_price = 120
         # затраты при толкании (руб/час)
         self.pushing_price = 30
-        # число толкачей
-        self.num_of_pushers = 3
-        # число вагонов в составе одного поезда (одинаковое для всех поездов?)
+        # общая сумма затрат
+        self.full_price = 0
+        # число вагонов в составе одного поезда (одинаковое для всех поездов)
         self.num_of_carriages = 6
-        # наработка толкача между техническим обслуживанием (час, только время толкания)
-        self.t_in_action = 1
-        # износ
-        self.deterioration = 0
         # время толкания (засунуть в функцию и будет увеличиваться)
         # 2 часа - туда, 1 час - обратно
-        self.t_pushing = 3
+        self.t_pushing = 2
         # время прицепа-отцепа толкача
         self.t_hitch_detach = 0.25
         # время движения толкача от начала участка толкания до депо (для прохождения ТО)
         # туда и обратно, едет от старта
-        self.t_pusher_to_depot = 2
+        self.t_to_depot = 2
         # время возврата толкача к началу участка толкания (после толкания на старт)
-        self.t_pusher_return = 1
+        self.t_return = 1
+        # процент износа толкача
+        self.deterioration_percentage = 1.03
+        # ТО
+        self.to = 3
 
-        # Пуассоновский поток случайных событий
-        # время потока (наблюдения) (в часах) , Tн
-        self.flow_time = 0
-        # интенсивность потока (изделия/24 часа), λ
-        self.intensity = 0
-        # интервал между случайными собитями , τ
-        self.interval = 0
-        # число событий за время наблюдения, N
-        self.N = 0
-        # время появления собития, m = 1/λ
-        self.m = 0
-        # равномерно распределенное от 0 до 1 случайное число,
-        self.r = 0
+        self.full_time = 0
+        self.pushers = None
 
+    def run(self, events):
+        # наработка толкача между техническим обслуживанием (час, только время толкания)
+        t_in_action = 0
+        # износ
+        deterioration = 1
+        # число толкачей
+        num_of_pushers = 2
 
+        # TODO сделать норм массив
+        self.pushers = np.zeros([1, num_of_pushers, 2])
+        print(self.pushers)
 
-    # износ толкача
-    def pusher_deterioration(self):
-        self.t_pushing *= self.deterioration
+        # dict_ = dict.fromkeys(['busy', 't_in_action', 'deterioration'], 0)
 
+        cur_t_event = events[0]
+        two_t_event = events[1]
 
-
-    def flow_init(self):
-        intensity = 8 / 24
-        flow_time = 200
         time_ = 0
-        count = 0
+        after_push_time = 0
 
-        while time_ <= flow_time:
-            r = generate_r()
-            time_interval = 1 / intensity * r
-            time_ = time_ + time_interval
-            count += 1
-            print(time_)
+        print(self.pushers)
 
-        print(count)
+        ret = self.push(self.pushers[0])
+        print(self.pushers)
 
+
+    def push(self, pusher):
+        pusher[0] = True
+
+        in_action = (self.t_hitch_detach + self.t_pushing + self.t_return) \
+                    * pusher[2]
+
+        pusher[1] += in_action
+        pusher[2] *= self.deterioration_percentage
+        pusher[0] = False
+
+        print(pusher)
+
+        self.full_time += pusher[1]
+        print(self.full_time)
+
+        return pusher
 
 s = Simulation()
-s.flow_init()
+ev = flow_init(8)
+s.run(ev)
+
+
